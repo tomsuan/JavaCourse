948dfb50f1688d92db60a75764f0e2529a5820cf
tomsuan <tom.murphy@me.com>
2016-12-30 09:48:49 +0000

    Green bottles with Scanner
    Added Scanner to Main

--- GreenBottles/src/Main.java
+++ GreenBottles/src/Main.java
@@ -1,12 +1,17 @@
-
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		int bottleAmt=9;
+		 Scanner reader = new Scanner(System.in);
+		 
+		 System.out.print("Enter number of bottles to start with: ");
+		 
+		int bott = Integer.parseInt(reader.nextLine());
+		//int bottleAmt=9;
 		String line="hanging on the wall";
 		String chorus="and if one green bottle should accidently fall";
-		for(int i=bottleAmt;i>0;i--){
+		for(int i=bott;i>0;i--){
 			if(i==1){
 				System.out.println("1 green bottle "+line);
 				System.out.println("1 green bottle "+line);
@@ -16,10 +21,10 @@ public class Main {
 				System.out.println(i+" green bottles "+line);
 				System.out.println(i+" green bottles "+line);
 				System.out.println(chorus);
-				System.out.println("they'll be "+(i-1)+" green bottles "+line);
+				System.out.println("there'll be "+(i-1)+" green bottles "+line);
 			}
 		}
-		System.out.println("they'll be no green bottles "
+		System.out.println("there'll be no green bottles "
 				+ "hanging on the wall");
 	}
 
